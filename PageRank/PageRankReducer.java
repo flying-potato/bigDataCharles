6ac2971d803e4fd3eef437ae7f3e3d16e4b18dc9
flying-potato <565881006@qq.com>
2016-09-27 16:01:28 -0400

    PageRank

---
+++ PageRank/PageRankReducer.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+public class PageRankReducer extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
+	public void reduce(Text key, Iterator<Text> values,OutputCollector<Text, Text> output, Reporter reporter)
+	throws IOException {
+		double finalPR = 0;
+		String des;
+		String result;
+		while (values.hasNext()) {
+			String eachvalue  = values.next();
+			try{
+				String[] sp = eachvalue.split(" ");
+				double eachPR =  Double.parseDouble(sp[1]);
+				finalPR += eachPR;
+			}catch (Exception e){
+				des = eachvalue; 
+			}
+		}
+		result = des + " " + Double.parseDouble(finalPR);
+		output.collect(key, new Text(maxValue));
+	}
+}
