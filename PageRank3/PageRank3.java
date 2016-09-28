5d39e2d9fed1ed2ae68cda60f7d3dc9ef91d46de
flying-potato <565881006@qq.com>
2016-09-27 22:44:03 -0400

    pagerank3

--- PageRank3/PageRank3.java
+++ PageRank3/PageRank3.java
@@ -8,23 +8,20 @@ import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 
-public class PageRank3 {
+public class PageRank {
 	public static void main(String[] args) throws IOException {
 		int i = 0;
 		while (i<3){
-
 			JobConf conf = new JobConf(PageRank3.class);
-			conf.setJobName("Page Rank 3");
+			conf.setJobName("Page Rank3");
 			FileInputFormat.addInputPath(conf, new Path(args[i+0]));
 			FileOutputFormat.setOutputPath(conf, new Path(args[i+1]));
 			conf.setMapperClass(PageRankMapper.class);
 			conf.setReducerClass(PageRankReducer.class);
 			conf.setOutputKeyClass(Text.class);
 			conf.setOutputValueClass(Text.class);
-			conf.setNumReduceTasks(1);
 			JobClient.runJob(conf);
 			i++;
 		}
-		
 	}
 }
