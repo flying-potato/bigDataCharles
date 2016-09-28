36f669704f58c422070d201a61c11f9ce2c1aeab
flying-potato <565881006@qq.com>
2016-09-27 21:59:16 -0400

    modification 3 time

--- PageRank/PageRank3.java
+++ PageRank/PageRank3.java
@@ -14,22 +14,28 @@ public class PageRank3 {
 			System.err.println("Usage: PageRank <input path> <output path>");
 			System.exit(-1);
 		}
-		Path path1 = new Path (args[0]);
-		Path path2 = new Path (args[1]);
-		for(int i = 1; i<=3; i++){
-			JobConf conf = new JobConf(PageRank3.class);
-			conf.setJobName("Page Rank 3 ");
+		int i=0;
+		while( i<3 ){
+			Job job = new Job();
+			Configuration conf = job.getConfiguration();
+			conf.set("mapreduce.output.textoutputformat.separator","");
+			job.setJarByClass(PageRank3.class);
+			job.setJobName("Page Rank 3 times");
 
+			// JobConf conf = new JobConf(PageRank3.class);
+			// conf.setJobName("Page Rank 3 ");
+			Path path1 = new Path (args[i+0]);
+			Path path2 = new Path (args[i+1]);
 			FileInputFormat.addInputPath(conf, path1);
 			FileOutputFormat.setOutputPath(conf, path2);
-			conf.setMapperClass(PageRankMapper.class);
-			conf.setReducerClass(PageRankReducer.class);
-			conf.setOutputKeyClass(Text.class);
-			conf.setOutputValueClass(Text.class);
-			JobClient.runJob(conf);
-			path1 = path2;
-			String temppath = path2.toString()+i;
-			path2 = new Path(temppath);
+			job.setNumReduceTasks(1);
+			job.setMapperClass(PageRankMapper.class);
+			job.setReducerClass(PageRankReducer.class);
+			job.setOutputKeyClass(Text.class);
+			job.setOutputValueClass(Text.class);
+			if(job.waitForCompletion(true)){
+				i++;
+			}
 		}
 	}
 }
