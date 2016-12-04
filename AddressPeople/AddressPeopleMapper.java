9dc3f69d3cacfd890c5048646b0a785a581a1bbb
flying-potato <565881006@qq.com>
2016-12-03 22:22:30 -0500

    key name revision

--- AddressPeople/AddressPeopleMapper.java
+++ AddressPeople/AddressPeopleMapper.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -12,14 +12,19 @@ implements Mapper<LongWritable, Text, Text, IntWritable> {
 	private static final int MISSING = 9999;
 	public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter)
 		throws IOException {
-		String line = value.toString();
+		try{
 
-		String[] sp = line.split(",");
-		String timeadd =  sp[0]+","+sp[1];
-		String peoplestr = sp[2];
-		int people = Integer.parseInt(peoplestr);
+			String line = value.toString();
 
-		output.collect(new Text(timeadd), new IntWritable(people));
+			String[] sp = line.split(",");
+			String timeadd =  sp[0]+","+sp[1];
+			String peoplestr = sp[2];
+			int people = Integer.parseInt(peoplestr);
+
+			output.collect(new Text(timeadd), new IntWritable(people));
+		}catch(Exception e){
+			System.out.println(e);
+		}
 	}
 
 }
